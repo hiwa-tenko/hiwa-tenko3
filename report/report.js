d51cfb067203660f82bd8d28de4e232444cc3c3e
osada mega <mega.osada.sf@gmail.com>
2025-08-28 23:38:23 +0900

    report.jsのexplodeをsplitに修正

--- report/report.js
+++ report/report.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const dateTimeString = record.start || record.end;
         if (!dateTimeString) return;
 
-        const recordDateStr = dateTimeString.explode('T')[0]; // "YYYY-MM-DD"
+        const recordDateStr = dateTimeString.split('T')[0]; // "YYYY-MM-DD"
         if (!dailyRecords[recordDateStr]) {
             dailyRecords[recordDateStr] = {};
         }
