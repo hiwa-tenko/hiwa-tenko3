fe6a998f8d8428e03d6b72939e016f8b80607d1b
osada mega <mega.osada.sf@gmail.com>
2025-08-30 12:27:09 +0900

    復元ページを削除

--- restore/restore.js
+++ restore/restore.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // script.jsから必要な定数をコピー
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw9WmBmnTBknedyvVgZ0HAyNYhTVzu9aesYue9GAP2GwMN_XbtzD9qJaHJC8SO_9yX8/exec';
+    //const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw9WmBmnTBknedyvVgZ0HAyNYhTVzu9aesYue9GAP2GwMN_XbtzD9qJaHJC8SO_9yX8/exec';
+    const API_URL = 'https://hiwa-tenko-backend.onrender.com/api/reports';  //supabase backend
+    
     const REPORT_HISTORY_KEY = 'reportHistory';
-    const DRIVER_NAME_KEY = 'driverName';
+    //const DRIVER_NAME_KEY = 'driverName';
 
     // restore.htmlにある要素を取得
     const restoreButton = document.getElementById('restoreButton');
-    const nameInput = document.getElementById('name');
-    const passwordInput = document.getElementById('password');
+    //const nameInput = document.getElementById('name');
+    //const passwordInput = document.getElementById('password');
     const messageDiv = document.getElementById('message');
 
     // ページ読み込み時に、前回入力した運転者名をフォームに設定
+    /*
     if (nameInput) {
         const savedName = localStorage.getItem(DRIVER_NAME_KEY);
         if (savedName) {
             nameInput.value = savedName;
         }
     }
-
+    */
     // 復元ボタンにクリックイベントを設定
-    if (restoreButton && nameInput && passwordInput && messageDiv) {
+    if (restoreButton) {
         restoreButton.addEventListener('click', () => {
+
+            //ここに、サーバ（GAS_WEB_APP_URL からsupabaseのAPI_URL）から
+            //ログインユーザの15日間のデータを取得する
+            //取得したデータをLocalStrageの履歴を上書き保存する
+
+            /*
             const driverName = nameInput.value.trim();
             const password = passwordInput.value;
 
@@ -36,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 messageDiv.className = 'error';
                 return;
             }
+
 
             // 処理中のメッセージを表示し、ボタンを無効化
             messageDiv.textContent = 'サーバーから履歴を復元しています...';
@@ -121,6 +131,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 // ボタンを再度有効化
                 restoreButton.disabled = false;
             });
+        */
         });
+
     }
 });
