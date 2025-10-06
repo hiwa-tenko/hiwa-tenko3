2c29f4c95f4059936ae437f1da886116a3d3e7ec
hiwa-tenko <mega.osada.sf@gmail.com>
2025-10-06 16:36:34 +0900

    backupDBを変更1

--- script.js
+++ script.js
@@ -259,7 +259,8 @@ const handleFormSubmit = async (e) => { // async関数に変更
     };
 
     // backup DB (rewritography.com/relait : MySQL)に保存　--start--
-    const backupAPI_URL = 'https://rewritography.com/relait/backupdb/backupdb_mysql.php';   // バックアップ用のURL
+    //const backupAPI_URL = 'https://rewritography.com/relait/backupdb/backupdb_mysql.php';   // バックアップ用のURL
+    const backendPHP_URL = 'https://qrepo.site/tenko_db/tenko_db_mysql.php';   // バックエンドPHP用のURL
 
     // バックアップAPIへの送信（メイン処理とは独立して実行）
     fetch(backupAPI_URL, {
