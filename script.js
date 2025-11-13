5d96025d0e0f8fac6ab48713de70489ddbaadcd0
hiwa_tenko <mega.osada.sf@gmail.com>
2025-11-13 12:07:31 +0900

    version = "0.877H";//20251113

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿// Hiwa点呼3
 //version H: HP, F: Fujitsu, A: AORUS, S: sykFujitsu
-const version = "0.876H";//20251113
+const version = "0.877H";//20251113
 //コミット例：　version = "0.873H";//20251113
 console.log("version=",version);
 document.getElementById('title_ver').textContent= "ver. " + version;
@@ -758,7 +758,7 @@ const setTenkoButton = () => {
             displayDurationTime();
             endTimeDiv.textContent= savedTenkoEnd;
             if(savedTenkoEnd === ""){   //終了時刻が無い場合（業務中ステータス）
-                statusText = "業務中"
+                statusText.textContent = "業務中";
             }
       
         } else if(savedStartEnd === "開始"){  //前回が開始点呼の場合
